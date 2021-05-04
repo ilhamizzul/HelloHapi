c1e0ea969cb37b224624e17d3a5a14a0ece5ecf6
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2021-05-04 09:01:00 +0700

    delete note

--- src/handler.js
+++ src/handler.js
@@ -98,9 +98,33 @@ const editNoteByIdHandler = (req, h) => {
   return response;
 };
 
+const deleteNoteByIdHandler = (req, h) => {
+  const { id } = req.params;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addNoteHandler,
   getAllNotesHandler,
   getNotesByIdHandler,
   editNoteByIdHandler,
+  deleteNoteByIdHandler,
 };
